17fa0c3f352a860739caa0111dda01b5356c8d1b
kaichaosun <kaichaosuna@gmail.com>
2024-06-09 00:36:56 +0800

    more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Price } from '@/components/price'
 import { Shortform } from '@/components/shortform'
 import { useState } from 'react'
+import { Cookie } from '@/components/cookie'
+import { Kanban } from './components/kanban'
+import { Terminal } from './components/terminal'
+import { Testimonial } from './components/testimonial'
+import { Music } from './components/music'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex flex-col items-center gap-10">
+    <div className="flex flex-col gap-10">
       <h1 className='mt-20'>Show v0</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -17,6 +22,16 @@ function App() {
       <Shortform />
       
       <Price />
+
+      <Cookie />
+
+      <Kanban />
+
+      <Terminal />
+
+      <Testimonial />
+
+      <Music />
     </div>
   )
 }
